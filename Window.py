6932d2500da67d1180d69b348e1110ae6090d38e
Faizan <mohammad.faizan.raja.25@gmail.com>
2026-06-27 11:46:07 -0400

    Built the basic pygame loop

---
+++ Window.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+SCREEN = pygame.display.set_mode((800,600))
+pygame.display.set_caption("PYGAME OS v0.1")
+CLOCK = pygame.time.Clock()
+FRAMERATE_CAP = 60
+RUNNING = True
+while RUNNING:
+    CLOCK.tick(FRAMERATE_CAP)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
+    
