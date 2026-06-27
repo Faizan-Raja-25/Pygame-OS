e41ab60930086c0d85edbb20c90ada2abc536c06
Faizan <mohammad.faizan.raja.25@gmail.com>
2026-06-27 11:59:44 -0400

    Noothing changed

--- MouseHover.py
+++ MouseHover.py
@@ -1,6 +1,12 @@
 import pygame
 import Window
 
+def printSlop(): {
+    print("Slop")
+}
+
 while Window.RUNNING:
+    Window.CLOCK.tick(60)
     mousePos = pygame.mouse.get_pos()
     print(pygame.mouse.get_pressed())
+    print("Ts workes")
